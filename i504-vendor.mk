f949f62d7b99aa3d7eb86e2d178502ffa945f530
Besik13 <egor.besik@gmail.com>
2015-11-16 16:46:51 +0200

    initial vendor

---
+++ i504-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nomi/i504/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nomi/i504/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nomi/i504/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nomi/i504/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/nomi/i504/xbin,system/xbin)
+
