5c05a0db6c697cc5b47d8fd94ad309199ee5a4aa
Besik13 <egor.besik@gmail.com>
2015-11-17 08:43:16 +0200

    Fix audio speaker

--- get_vendor.sh
+++ get_vendor.sh
@@ -22,7 +22,7 @@ GL="/lib/egl/libGLESv1_CM_mali.so /lib/egl/libGLESv2_mali.so /lib/egl/libEGL_mal
 /lib/libm4u.so /lib/hw/hwcomposer.mt6580.so /lib/hw/gralloc.mt6580.so \
 /lib/libdpframework.so /lib/libion.so /lib/libMali.so /lib/libged.so /lib/libgpu_aux.so /lib/libmtk_mali_user.so \
 /lib/libgralloc_extra.so /lib/libion_mtk.so /lib/libpq_prot.so /lib/libbwc.so /lib/libgui_ext.so /lib/libperfservicenative.so \
-/lib/libui_ext.so /lib/libui.so /lib/libdrmmtkutil.so /lib/libdrmmtkwhitelist.so /lib/libdrmmtkutil.so"
+/lib/libui_ext.so /lib/libui.so /lib/libdrmmtkutil.so /lib/libdrmmtkwhitelist.so /lib/libdrmmtkutil.so /bin/guiext-server"
 
 #
 # ccci_mdinit starts, depends on additional services:
@@ -42,17 +42,19 @@ RIL="/lib/mtk-ril.so /lib/librilmtk.so /lib/libaed.so /bin/mtkrild /bin/mtkrildm
 /bin/atcid /bin/atci_service /lib/libccci_util.so /bin/md_ctrl"
 
 AUDIO="/bin/audiocmdservice_atci /lib/libblisrc.so /lib/libspeech_enh_lib.so /lib/libaudiocustparam.so /lib/libaudiosetting.so \
-/lib/libaudiocompensationfilter.so /lib/libcvsd_mtk.so /lib/libmsbc_mtk.so /lib/libaudiocomponentengine.so \
+/lib/libcvsd_mtk.so /lib/libmsbc_mtk.so /lib/libaudiocomponentengine.so \
 /lib/libblisrc32.so /lib/libbessound_hd_mtk.so /lib/libmtklimiter.so /lib/libmtkshifter.so /lib/libaudiodcrflt.so \
-/lib/libbluetoothdrv.so /lib/hw/audio.usb.default.so /lib/libtinycompress.so /lib/libtinyxml.so /lib/libMtkOmxCore.so \
-/lib/libMtkOmxVenc.so /lib/libMtkOmxVdec.so /etc/mtk_omx_core.cfg /lib/libaudiomtkdcremoval.so /lib/hw/audio.primary.mt6580.so \
-/lib/libmtkplayer.so /lib/libvcodecdrv.so /lib/libvcodec_utility.so /lib/libssladp.so /lib/libmhalImageCodec.so /lib/libvcodec_oal.so  /lib/libvcodec_oal.so"
+/lib/libbluetoothdrv.so /lib/hw/audio.usb.default.so /lib/libtinycompress.so /lib/libtinyxml.so /etc/mtk_omx_core.cfg /lib/libaudiomtkdcremoval.so /lib/hw/audio.primary.mt6580.so \
+/lib/libmtkplayer.so /lib/libvcodecdrv.so /lib/libvcodec_utility.so /lib/libssladp.so /lib/libmhalImageCodec.so /lib/libvcodec_oal.so  /lib/libvcodec_oal.so \
+/lib/libmmprofile_jni.so /lib/libmmprofile.so /lib/libJpgDecPipe.so /lib/libGdmaScalerPipe.so /lib/libSwJpgCodec.so \
+/lib/libMtkOmxAdpcmDec.so /lib/libMtkOmxAlacDec.so /lib/libMtkOmxCore.so /lib/libMtkOmxG711Dec.so /lib/libMtkOmxMp3Dec.so /lib/libMtkOmxVdec.so /lib/libMtkOmxVorbisEnc.so \
+/lib/libMtkOmxAdpcmEnc.so /lib/libMtkOmxApeDec.so /lib/libMtkOmxFlacDec.so /lib/libMtkOmxGsmDec.so /lib/libMtkOmxRawDec.so /lib/libMtkOmxVenc.so"
 
 CAMERA="/lib/libcam_mmp.so /lib/libcam.utils.so /lib/libfeatureio.so /lib/libcam.metadataprovider.so /lib/libcam.utils.sensorlistener.so \
 /lib/libcam3_app.so /lib/libcam.halsensor.so /lib/libcameracustom.so /lib/hw/camera.mt6580.so /lib/libcam_platform.so /lib/libcam.iopipe_FrmB.so \
-/lib/libcam.camshot.so /lib/libcam.exif.so /lib/libcam.exif.v3.so /lib/libcam.paramsmgr.so lib/libcam.device3.so /lib/libcam3_hwnode.so \
-/lib/libcam.client.so /lib/libcam_utils.so /lib/libcam3_pipeline.so lib/libcam3_utils.so /lib/libcam.camadapter.so /lib/libcam.iopipe.so \
-/lib/libcam.device1.so lib/libcam.metadata.so /lib/libcamdrv.so lib/libcam.campipe.so /lib/libcam.hal3a.v3.so /lib/libmtkjpeg.so \
+/lib/libcam.camshot.so /lib/libcam.exif.so /lib/libcam.exif.v3.so /lib/libcam.paramsmgr.so /lib/libcam.device3.so /lib/libcam3_hwnode.so \
+/lib/libcam.client.so /lib/libcam_utils.so /lib/libcam3_pipeline.so /lib/libcam3_utils.so /lib/libcam.camadapter.so /lib/libcam.iopipe.so \
+/lib/libcam.device1.so /lib/libcam.metadata.so /lib/libcamdrv.so /lib/libcam.campipe.so /lib/libcam.hal3a.v3.so /lib/libmtkjpeg.so \
 /lib/libcamdrv_FrmB.so /lib/libcamalgo.so /lib/libcam3_hwpipeline.so /lib/libcam_hwutils.so /lib/libcamera_client_mtk.so /lib/libmmsdkservice.so \
 /lib/libmtk_mmutils.so /lib/libcam.sdkclient.so /lib/libmmsdkservice.feature.so /lib/libcam1_utils.so /lib/libJpgEncPipe.so /lib/libimageio.so \
 /lib/libmpo.so /lib/libmpoencoder.so /lib/libmpodecoder.so /lib/libmpojni.so /lib/libmpo.so /lib/lib3a.so /lib/libfeatureiodrv.so /lib/libimageio_plat_drv.so \
@@ -65,7 +67,7 @@ XBIN="/xbin/BGW /xbin/mnld"
 OTHER="/bin/akmd8963 /bin/akmd8975 /bin/akmd09911 /bin/ami304d /bin/bmm050d /bin/mc6420d /bin/s62xd \
 /lib/hw/sensors.mt6580.so /lib/hw/lights.default.so /bin/mtk_agpsd /bin/muxreport /bin/pppd_dt /bin/pppd \
 /bin/memsicd3416x /bin/memsicd /lib/hw/memtrack.mt6580.so /bin/batterywarning /bin/thermal_manager /bin/lsm303md /lib/libbluetoothem_mtk.so /lib/libbluetooth_relayer.so \
-/lib/libmnl.so /lib/libstagefrighthw.so /lib/libmtcloader.so /bin/permission_check"
+/lib/libmnl.so /lib/libstagefrighthw.so /lib/libmtcloader.so /bin/permission_check /bin/xlog"
 
 SYSTEM="$FIRMWARE $WIFI $GL $RIL $AUDIO $OTHER $VENDOR $XBIN $CAMERA"
 
